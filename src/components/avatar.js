cc8233906d2e72c4c39ab4ca57b1ea474b066ff0
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-17 12:24:33 -0800

    Update avatar.js

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -26,7 +26,7 @@ const Avatar = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} className="logo-avatar" />
 }
 
 export default Avatar
