18cd272d08a83ccb78c383df6f1080ea3c1727b3
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-17 11:50:07 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,13 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div style={{ marginBottom: 50 }}>
       <h1>Hello, I'm Thant!</h1>
-      <p>I'm a software developer from Vancouver. I have 11 years of .Net development experience. I am originally from Myanmar. I moved to Singapore to study when I was 17. I worked in Singapore for 11 years before migrating to Canada.</p>
+      <ul>
+        <li>a software developer from Vancouver</li>
+        <li>11 years of .Net development experience</li>
+        <li>originally from Myanmar</li>
+        <li>moved to Singapore to study when I was 17</li>
+        <li>worked in Singapore for 11 years before migrating to Canada</li>
+      </ul>
       Check out my <Link to="/blog/">blog posts</Link>. I write mostly in Burmese.
     </div>
   </Layout>
