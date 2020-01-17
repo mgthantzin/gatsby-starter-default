6e600bdf3fdbcca143d374b9db22e1505e862539
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-17 12:15:50 -0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import { Location } from "@reach/router"
-import Image from "../components/avatar"
+import Avatar from "../components/avatar"
 
 const Header = ({ avatar }) => (
   <header>
