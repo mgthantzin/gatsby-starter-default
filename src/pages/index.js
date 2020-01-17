c70fd659d7f29e3eef2c029f82ae81cc344f415d
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-17 11:45:47 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello, I'm Thant!</h1>
-    <p>I'm a software developer from Vancouver. I have 11 years of .Net development experience. I am originally from Myanmar. I moved to Singapore to study when I was 17. I worked in Singapore for 11 years before migrating to Canada.</p>
-    Check out my <Link to="/blog/">blog posts</Link>. I write mostly in Burmese.
+    <div style={{ marginBottom: 50 }}>
+      <h1>Hello, I'm Thant!</h1>
+      <p>I'm a software developer from Vancouver. I have 11 years of .Net development experience. I am originally from Myanmar. I moved to Singapore to study when I was 17. I worked in Singapore for 11 years before migrating to Canada.</p>
+      Check out my <Link to="/blog/">blog posts</Link>. I write mostly in Burmese.
+    </div>
   </Layout>
 )
 
