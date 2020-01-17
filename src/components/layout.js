f339e2ff8dfeab7d060eb0fed13f90f6c4e9dc30
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-17 11:05:54 -0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer className="mt-50">
+        <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
