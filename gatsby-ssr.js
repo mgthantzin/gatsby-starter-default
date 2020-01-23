de3bcc80f63efce84fc09973846393e6e5b8cd23
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-23 13:24:30 -0800

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -17,7 +17,17 @@ setPreBodyComponents([
 ]);
  
 setPostBodyComponents([
-	//
+	<script>
+	(function() {
+		window.counter = 'https://tzo.goatcounter.com/count'
+
+		var script = document.createElement('script');
+		script.async = 1;
+		script.src = '//gc.zgo.at/count.js';
+		var ins = document.getElementsByTagName('script')[0];
+		ins.parentNode.insertBefore(script, ins)
+	})();
+	</script>
 ]);
 
 }
