454d8600775edf65d677e7d048329cb764af3e03
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-23 09:20:59 -0800

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
+export function onRenderBody(
+  { setHeadComponents, setPreBodyComponents, setPostBodyComponents }) {
+setHeadComponents([
+  //
+]);
+ 
+setPreBodyComponents([
+	//
+]);
+ 
+setPostBodyComponents([
+	<script>
+    dangerouslySetInnerHTML={{
+        __html=`
+        (function() {
+          window.counter = 'https://tzo.goatcounter.com/count'
+
+          var script = document.createElement('script');
+          script.async = 1;
+          script.src = '//gc.zgo.at/count.js';
+          var ins = document.getElementsByTagName('script')[0];
+          ins.parentNode.insertBefore(script, ins)
+        })();
+   	 `
+     }}
+  </script>
+]);
+
+}
