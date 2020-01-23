fb5f2858fd6704b2421b8a6e5e4cbaf757463eba
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-01-23 13:26:11 -0800

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,29 +5,19 @@
  */
 
 // You can delete this file if you're not using it
-import React from "react";
-export function onRenderBody(
-  { setHeadComponents, setPreBodyComponents, setPostBodyComponents }) {
-setHeadComponents([
-  //
-]);
- 
-setPreBodyComponents([
-	//
-]);
- 
-setPostBodyComponents([
-	<script>
-	(function() {
-		window.counter = 'https://tzo.goatcounter.com/count'
+const React = require("react");
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    	<script>
+		(function() {
+			window.counter = 'https://tzo.goatcounter.com/count'
 
-		var script = document.createElement('script');
-		script.async = 1;
-		script.src = '//gc.zgo.at/count.js';
-		var ins = document.getElementsByTagName('script')[0];
-		ins.parentNode.insertBefore(script, ins)
-	})();
+			var script = document.createElement('script');
+			script.async = 1;
+			script.src = '//gc.zgo.at/count.js';
+			var ins = document.getElementsByTagName('script')[0];
+			ins.parentNode.insertBefore(script, ins)
+		})();
 	</script>
-]);
-
-}
+  ]);
+};
