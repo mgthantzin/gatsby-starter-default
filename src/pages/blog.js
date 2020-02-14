382f5cf19741ba43546e2947b3d361a67e108dcd
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-02-14 10:19:01 -0800

    Reverted last change

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -18,7 +18,7 @@ const BlogPage = ({ data }) => {
     posts.map(post => (
       <li key={post.node.wordpress_id}>
         <div className="post-date code">
-          {post.node.date.substring(0, 6)}
+          <small>{post.node.date.substring(0, 6)}</small>
         </div>
         <div className="title">
           <Link to={decodeURI(`/posts/${post.node.slug}/`)}>{he.decode(post.node.title)}</Link>
