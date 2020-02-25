f5dd8c78950ca0b8aa14aae147c9133f87782dcd
Thant Zin Oo <137266+mgthantzin@users.noreply.github.com>
2020-02-25 14:17:37 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,24 @@ import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = ({ data }) => (
+  const posts = data.allWordpressPost.edges.filter(
+      p => p.node.date !== null
+  )
+
+  const postsList = posts =>
+    posts.map(post => (
+      <li key={post.node.wordpress_id}>
+        <div className="post-date code">
+          <small>{post.node.date}</small>
+        </div>
+        <div className="title">
+          <Link to={decodeURI(`/posts/${post.node.slug}/`)}>{he.decode(post.node.title)}</Link>
+        </div>
+      </li>
+    ))
+
+  return(<Layout>
     <SEO title="Home" />
     <div style={{ marginBottom: 50 }}>
       <h1>Hello, I'm <span style={{ color: '#005D8F' }}>Thant</span>!</h1>
@@ -18,8 +34,27 @@ const IndexPage = () => (
         <li>worked in Singapore for 11 years before migrating to Canada</li>
       </ul>
       <p>Check out my <Link to="/blog/">blog posts</Link>. I write mostly in Burmese. Sometimes, I also share articles that I find interesting. This website has tracking enabled for minimal analytics. Please refer to <Link to="/privacy/">Privacy Policy</Link>.</p>
+      <p>
+         Recently, I wrote<br />
+         {postsList(posts)}
+      </p>
     </div>
-  </Layout>
+  </Layout>)
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allWordpressPost(filter: {status: {eq: "publish"}}, sort: {order: DESC, fields: [date]}, limit: 5) {
+          edges {
+              node {
+                  date
+                  title
+
+                  slug
+              }
+          }
+      }
+  }
+`
